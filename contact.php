38b73eaa106195496907f9738098e79d8675b39b
Rahul N Bangera <85377350+Bnir@users.noreply.github.com>
2023-10-21 09:44:25 +0530

    Added PHP (UNDER TESTING)
    Integrated PHP such that the contact us Form data to be Added
    to MYSQL
    NOT THE FINAL EDIT FOR PHP FILE (Contact.php), STILL UNDER TESTING

--- contact.php
+++ contact.php
@@ -1,3 +1,30 @@
+<?php
+if (isset($_POST['name'])) {
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $conn = new mysqli($server, $username, $password);
+
+    if (!$conn) {
+        die("Connection is failed due to " . mysqli_connect_error());
+    }
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['sub'];
+    $desc = $_POST['desc'];
+
+    $sql = "INSERT INTO `contact` (`Name`, `Email ID`, `Subject`, `Description`, `Date and Time`) VALUES ('$name', '$email', '$subject', '$desc', current_timestamp());";
+
+    if ($conn->query($sql)) {
+        echo "Submitted Successfully";
+    } else
+        echo "ERROR: " . $conn->error();
+
+    $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,12 +37,12 @@
 
 <body class="contacthead" style="background-color: blanchedalmond; font-weight: bold;">
     <h1 style="text-align: center; background-color: rgb(154, 164, 228);"><u>Contact US</u></h1>
-    <form class="contact" action="action.html">
+    <form class="contact" action="contact.php" method="POST">
         <label for="name">Your Name</label>
         <input type="text" name="name" id="name" placeholder="Name" required>
         <label for="email">Email ID</label>
-            <input type="email" name="email" id="email" placeholder="Email ID">
-            <br>
+        <input type="email" name="email" id="email" placeholder="Email ID">
+        <br>
 
         <label for="subject">Subject:</label>
         <input type="text" name="subject" id="subject" placeholder="Subject">
