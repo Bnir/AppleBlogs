e657f7ca146c82f2cc4bf59776fd160a9ab1ce51
Rahul N Bangera <85377350+Bnir@users.noreply.github.com>
2023-10-22 20:03:27 +0530

    TRIAL AND ERROR 1 (DUE TO DEPLOY ERROR) THIS IS A FAKE COMMIT BEING DONE TO CHECK WHATS WRONG

--- contact.php
+++ contact.php
@@ -77,8 +77,8 @@ if (isset($_POST['fname'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iphone 15 Ultra</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
